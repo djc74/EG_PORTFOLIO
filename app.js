5eb5d57a2c8816178293f803171f1773fd43782b
djc74 <davidcooper974@gmail.com>
2018-03-27 22:25:58 +0100

    carousel started

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
+//sidenav
+
 function openNav() {
   document.getElementById("mySideNav").style.width = "250px";
 }
 
 function closeNav() {
   document.getElementById("mySideNav").style.width = "0px";
+}
+
+//carousel
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
 }
