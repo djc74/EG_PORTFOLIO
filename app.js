8e428b3d9d0b57d8248c8d879c9dc2bf5ef0c9e9
djc74 <davidcooper974@gmail.com>
2018-03-29 15:00:23 +0100

    carousel working

--- app.js
+++ app.js
@@ -1,34 +1,31 @@
-//sidenav
+  //sidenav
 
-function openNav() {
-  document.getElementById("mySideNav").style.width = "250px";
-}
-
-function closeNav() {
-  document.getElementById("mySideNav").style.width = "0px";
-}
-
-//carousel
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+  function openNav() {
+    document.getElementById("mySideNav").style.width = "250px";
   }
-  slides[slideIndex-1].style.display = "block";
-}
+
+  function closeNav() {
+    document.getElementById("mySideNav").style.width = "0px";
+  }
+
+  //carousel
+
+  var slideIndex = 1;
+  window.addEventListener('DOMContentLoaded', () => {
+    showSlides(slideIndex);
+    });
+
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+  }
